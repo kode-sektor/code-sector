c8fd8edc366bb2b2b9a51f2b9b4c909eda76097c
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-10-28 02:15:36 -0400

    Add gallery section

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,54 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
 
-const gallery = () => {
+const items = [
+    {
+        altText: 'Slide 1',
+        caption: <p><span class="animated swing">With <b>great coding</b></span><span className="animated bounceInLeft"> comes <b>great responsibility</b></span></p>,
+        header: 'Slide 1 Header',
+        key: '1'
+    },
+    {
+        altText: 'Slide 2',
+        caption: <p><span class="animated wobble">If you can't <b>create</b> it; </span><span class="animated bounceInLeft"><b>create</b> the illusion</span></p>,
+        header: 'Slide 2 Header',
+        key: '2'
+    },
+    {
+        altText: 'Slide 3',
+        caption: <p><span class="animated wobble">If you can't <b>create</b> it; </span><span class="animated bounceInLeft"><b>create</b> the illusion</span></p>,
+        header: 'Slide 3 Header',
+        key: '3'
+    },
+    {
+        altText: 'Slide 3',
+        caption: <p><span class="animated rotateOutDownRight">Web <b>programming</b></span><span class="animated bounceInLeft"> on <b>another level</b></span></p>,
+        header: 'Slide 3 Header',
+        key: '3'
+    }
+];
+
+const Gallery = () => {
     return (
-        <section id="main-carousel" class="jumbotron">
-
-            <div class="carousel slide container" id="screenshot-carousel" data-ride="carousel">
-
-                <ol class="carousel-indicators">
-                    <li data-target="#screenshot-carousel" data-slide-to="0"></li>
-                    <li data-target="#screenshot-carousel" data-slide-to="1"></li>
-                    <li data-target="#screenshot-carousel" data-slide-to="2"></li>
-                    <li data-target="#screenshot-carousel" data-slide-to="3"></li>
-                </ol>
-
-                <div class="carousel-inner">
-
-                    <p class="author"><span>code</span> <span>sector</span></p>                    
-                    <div class="carousel-item active">
-                        <div class="carousel-caption">
-                            <p><span class="animated tada"><b>Uncommon</b> things</span><span class="animated bounceInLeft"> in <b>common ways</b></span></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="carousel-caption">
-                            <p><span class="animated swing">With <b>great coding</b></span><span class="animated bounceInLeft"> comes <b>great responsibility</b></span></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="carousel-caption">
-                            <p><span class="animated wobble">If you can't <b>create</b> it; </span><span class="animated bounceInLeft"><b>create</b> the illusion</span></p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="carousel-caption">
-                            <p><span class="animated rotateOutDownRight">Web <b>programming</b></span><span class="animated bounceInLeft"> on <b>another level</b></span></p>
-                        </div>
-                    </div>
-                </div>
-
-                <a href="#screenshot-carousel" class="carousel-control-prev left carousel-control" data-slide="prev">
-                    <span class="fa-4x fa fa-angle-left"></span>
-                </a>
-                <a href="#screenshot-carousel" class="carousel-control-next right carousel-control" data-slide="next">
-                    <span class="fa-4x fa fa-angle-right"></span>
-                </a>
-
-            </div>
-
+        <section id="main-carousel" className="jumbotron main-carousel">
+            <UncontrolledCarousel items={items} />
         </section>
     )
-}
+};
 
-export default gallery
+export default Gallery;
