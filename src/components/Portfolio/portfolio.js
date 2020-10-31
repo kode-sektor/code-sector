3c5c1a7941fd6e8cf98bc4f3c92dbf9a80fa2d33
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-10-31 09:24:08 -0400

    Display category options

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,49 +1,102 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {client} from '../../client'
 import Header from '../header'
 import Footer from '../footer'
 import WorkTogether from '../work-together'
+import { FaGithub } from 'react-icons/fa'
 
-const portfolio = () => {
 
-    return (
+class portfolio extends React.Component{
 
-        <section id="portfolio-single-page" class="page-section">
+    state = {
+        portfolio : [],
+        categories : []
+    }
 
-            <Header />
+    getCategories = (items) => {
+        let tempItems = items.map(item => {
+            // const {caption, githubLink, link, category, multipleCategories, poster } = item.fields
+            const fields = item.fields
+            let {category, multipleCategories} = fields
+            console.log (multipleCategories);
 
-            <div id="portfolio-detail" class="container sheet code">
+            if (multipleCategories) {
+                if (Array.isArray(multipleCategories)) {
+                    return multipleCategories   // returns array into tempItems array
+                }
+            }
+             else {
+                if (category) return category
+            }
+        })
+        let categories = Array.from(new Set(tempItems.flat())); // spreads nested multipleCategories array
+        categories = ['all', ...categories]
+        return categories
+    }
 
-                <section class="portfolio-grid row">
-                    <div class="portfolio-grid-link col-sm-6 github-adidas">
-                        <a rel="external" target="_blank" class="portfolio-link-ext" href="https://kode-sektor.github.io/landing-page-adidas-psd-to-html/index.html">
-                            <span class="link-ext-title">ADIDAS LANDING PAGE</span>
-                        </a>
-                    </div>
-                    <div class="portfolio-grid-link col-sm-6 github-humber-recreation">
-                        <a rel="external" target="_blank" class="portfolio-link-ext" href="https://kode-sektor.github.io/wddm-120-psd-html-layout/">
-                            <span class="link-ext-title">HUMBER RECREATION LAYOUT</span>
-                        </a>
-                    </div>
-                    <div class="portfolio-grid-link col-sm-6 github-gidifest">
-                        <a rel="external" target="_blank" class="portfolio-link-ext" href="https://kode-sektor.github.io/wddm-121-gidifest-microsite/index.html">
-                            <span class="link-ext-title">GIDIFEST MICROSITE</span>
-                        </a>
-                    </div>
-                    <div class="portfolio-grid-link col-sm-6 github-airbnb">
-                        <a rel="external" target="_blank" class="portfolio-link-ext" href="https://kode-sektor.github.io/application-interface-redesign/source-codes/index.htm">
-                            <span class="link-ext-title">AIRBNB REDESIGN</span>
-                        </a>
-                    </div>
-                </section>
+    componentDidMount ()  {
+        client.getEntries({'content_type' : 'codeSectorPortfolio'}).then((response) => {
+            this.setState({
+                portfolio : response.items,
+                categories : this.getCategories(response.items)
+            })
+        }).catch(console.error)
+    }
 
-            </div>
+    render () {
+        console.log(this.state.categories)
 
-            <WorkTogether />
+        return (
 
-            <Footer />
+            <section id="portfolio-single-page" className="page-section">
+    
+                <Header />
 
-        </section>
-    )
+                <div className="select">
+                    <select>
+                        <option selected disabled>Choose an option</option>
+                        {this.state.categories.map((category) => {
+                            return (<option>{category}</option>)
+                        })}
+                    </select>
+                </div>
+
+                <div id="portfolio-detail" className="container sheet code">
+    
+                    <section className="portfolio-grid row">
+                        <div className="portfolio-grid-link col-sm-6 github-adidas">
+                            <a rel="external" href="https://kode-sektor.github.io/landing-page-adidas-psd-to-html/index.html" target="_blank" className="portfolio-link-ext">
+                                <span className="link-ext-title">ADIDAS LANDING PAGE</span>
+                            </a>
+                            <a rel="external" href="https://kode-sektor.github.io/landing-page-adidas-psd-to-html/index.html" target="_blank"><FaGithub /></a>
+                        </div>
+                        <div className="portfolio-grid-link col-sm-6 github-humber-recreation">
+                            <a rel="external" target="_blank" className="portfolio-link-ext" href="https://kode-sektor.github.io/wddm-120-psd-html-layout/">
+                                <span className="link-ext-title">HUMBER RECREATION LAYOUT</span>
+                            </a>
+                        </div>
+                        <div className="portfolio-grid-link col-sm-6 github-gidifest">
+                            <a rel="external" target="_blank" className="portfolio-link-ext" href="https://kode-sektor.github.io/wddm-121-gidifest-microsite/index.html">
+                                <span className="link-ext-title">GIDIFEST MICROSITE</span>
+                            </a>
+                        </div>
+                        <div className="portfolio-grid-link col-sm-6 github-airbnb">
+                            <a rel="external" target="_blank" className="portfolio-link-ext" href="https://kode-sektor.github.io/application-interface-redesign/source-codes/index.htm">
+                                <span className="link-ext-title">AIRBNB REDESIGN</span>
+                            </a>
+                        </div>
+                    </section>
+    
+                </div>
+    
+                <WorkTogether />
+    
+                <Footer />
+    
+            </section>
+        )
+    }
+
 }
 
 export default portfolio
